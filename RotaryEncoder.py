c58757fcdfe82dd9d11ecf6de25a3bd6f19022ac
John Baboval <baboval@gmail.com>
2019-03-24 01:43:15 +0000

    Change default from CHIP.io to RPi.GPIO Change interface to allow one instance per encoder board   (GPIO/I2C objects become class attributes)

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -1,66 +1,93 @@
 #! /usr/bin/python
-
-import CHIP_IO.GPIO as GPIO
+import RPi.GPIO as GPIO
+import Adafruit_GPIO.I2C as I2C
 import threading
 import atexit
 import time
-import Adafruit_GPIO.I2C as I2C
 import struct
 
 class RotaryEncoder():
 
-  def __init__(self, addresses, invert=False, factor=1):
-    atexit.register(GPIO.cleanup)
+  registered = False
+  encoders = {}
 
+  def __init__(self, address, invert=False, factor=1):
+    if RotaryEncoder.registered is False:
+      RotaryEncoder.registered = True
+      atexit.register(GPIO.cleanup)
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(17, GPIO.IN)
+      GPIO.add_event_detect(17, GPIO.FALLING, callback=RotaryEncoder._interrupt)
+
+    self.address = address
     self.factor = factor
     if invert:
-        self.factor *= -1
+      self.factor *= -1
 
-    self.encoders = {}
-    for address in addresses:
-      self.encoders[address] =  I2C.get_i2c_device(address, busnum=1)
+    if address in RotaryEncoder.encoders:
+      raise Exception("Encoder at that address already registered")
 
-    self.intr = "XIO-P0"
-    GPIO.setup(self.intr, GPIO.IN)
-
+    self.device = I2C.get_i2c_device(address, busnum=1)
     self.residual = 0
+    RotaryEncoder.encoders[address] = self
+
+  def __del__(self):
+    del RotaryEncoder.encoders[self.address]
+
+  @classmethod
+  def _interrupt(cls, pin):
+    for encoder in cls.encoders.keys():
+      cls.encoders[encoder]._call_callbacks(pin)
 
   def _call_callbacks(self, pin):
-    for address, encoder in self.encoders.iteritems():
-      pips = encoder.readS8(0)
-      presses = encoder.readU8(1)
+    try:
+      pips = self.device.readS8(0)
+    except IOError as e:
+      pips = 0
+      self.device.readS8(0)
 
-      pips *= self.factor
-      pips += self.residual
+    try: 
+      presses = self.device.readU8(1)
+    except IOError as e:
+      presses = 0
+      self.device.readU8(1)
 
-      if abs(pips) < 1:
-          self.residual = pips
-          pips = 0
-      else:
-          self.residual = pips % 1
-          pips -= self.residual
+    # Filter
+    if abs(pips) > 14:
+      pips = 0
+    if presses > 3:
+      presses = 0
 
-      if pips != 0 and self.turn_cb is not None:
-        self.turn_cb(address, pips)
+    pips *= self.factor
+    pips += self.residual
 
-      if presses != 0 and self.press_cb is not None:
-        self.press_cb(address, presses)
+    if abs(pips) < 1:
+      self.residual = pips
+      pips = 0
+    else:
+      self.residual = pips % 1
+      pips -= self.residual
+
+    if pips != 0 and self.turn_cb is not None:
+      self.turn_cb(self.address, pips)
+
+    if presses != 0 and self.press_cb is not None:
+      self.press_cb(self.address, presses)
 
   def register_callbacks(self, turn=None, press=None):
     self.turn_cb = turn
     self.press_cb = press
-    GPIO.add_event_detect(self.intr, GPIO.FALLING, callback=self._call_callbacks)
 
-  def read_switches(self, pin=0):
-    for address, encoder in self.encoders.iteritems():
-      pips = encoder.readS8(0)
-      presses = encoder.readU8(1)
-      if pips:
-          self.turn_cb(address, pips)
-      if presses:
-          self.press_cb(address, presses)
+  def clear_switches(self, pin=0):
+    try:
+      self.device.readS8(0)
+    except IOError as e:
+      self.device.readS8(0)
 
-      return (pips, presses)
+    try: 
+      self.device.readU8(1)
+    except IOError as e:
+      self.device.readU8(1)
 
 def _press_cb(address, presses):
     print "Button %d pressed %d times" % (address, presses)
@@ -69,10 +96,18 @@ def _turn_cb(address, pips):
     print "Knob %d turned %d pips" % (address, pips)
 
 if __name__ == '__main__':
-  re = RotaryEncoder([0x10, 0x11, 0x12], invert=True, factor=0.5)
-  re.read_switches()
-  re.register_callbacks(turn=_turn_cb, press=_press_cb)
+  re = {}
+  re[0x10] = RotaryEncoder(0x10, invert=True, factor=0.5)
+  re[0x10].clear_switches()
+  re[0x10].register_callbacks(turn=_turn_cb, press=_press_cb)
+  re[0x11] = RotaryEncoder(0x11, invert=True, factor=0.5)
+  re[0x11].clear_switches()
+  re[0x11].register_callbacks(turn=_turn_cb, press=_press_cb)
+  re[0x12] = RotaryEncoder(0x12, invert=False, factor=0.5)
+  re[0x12].clear_switches()
+  re[0x12].register_callbacks(turn=_turn_cb, press=_press_cb)
+  re[0x13] = RotaryEncoder(0x13, invert=False, factor=0.5)
+  re[0x13].clear_switches()
+  re[0x13].register_callbacks(turn=_turn_cb, press=_press_cb)
   while True:
-    re.read_switches(0)
-#    print "Intr: %d" % GPIO.input(re.intr) 
     time.sleep(1)
